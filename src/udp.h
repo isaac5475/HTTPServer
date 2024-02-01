555e988f3c78b6582ed166de648b9578a75ce84c
murat <murat_zaydullin@campus.tu-berlin.de>
2024-02-01 12:56:19 +0100

    extracted metods forward_packet and successor_is_responsible

--- src/udp.h
+++ src/udp.h
@@ -13,4 +13,6 @@
 void udp_handler(uint8_t* buff, int fd, struct sockaddr* ipaddr, socklen_t addr_len, struct data* data, struct dht* dht);
 void create_msg(uint8_t* packet, uint8_t msg_type, uint16_t hash_id, uint16_t node_id, struct in_addr* node_ip, uint16_t node_port);
 void send_msg(int fd, uint8_t* msg, struct sockaddr_in* node_addr);
+void forward_packet(const uint8_t *buff, int fd, const struct dht *dht);
+uint8_t successor_is_responsible(const struct data *data, uint16_t id);
 #endif //RN_PRAXIS_UDP_H
