c343ea8227e5551263362ac84985e0f57d3c49af
murat <murat_zaydullin@campus.tu-berlin.de>
2024-02-01 12:56:42 +0100

    join signal implemented

--- src/udp.h
+++ src/udp.h
@@ -8,7 +8,8 @@
 #include "utils.h"
 
 #define LOOKUP_MODE 0
-#define RESULT_MODE 1
+#define REPLY_MODE 1
+#define NOTIFY_MODE 3
 #define JOIN_MODE 4
 void udp_handler(uint8_t* buff, int fd, struct sockaddr* ipaddr, socklen_t addr_len, struct data* data, struct dht* dht);
 void create_msg(uint8_t* packet, uint8_t msg_type, uint16_t hash_id, uint16_t node_id, struct in_addr* node_ip, uint16_t node_port);
